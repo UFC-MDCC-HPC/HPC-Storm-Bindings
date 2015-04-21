278ff56d28faa0c457089c680f6e79cc3fce7e26
heron-carvalho <carvalho.heron@gmail.com>
2015-04-21 00:55:22 -0300

    updates

---
+++ br.ufc.mdcc.hpc.storm.binding.context.ActionInterface/src/1.0.0.0/IActionInterfaceProvider.cs
@@ -0,0 +1,11 @@
+using br.ufc.pargo.hpe.kinds;
+
+namespace br.ufc.mdcc.hpc.storm.binding.context.ActionInterface { 
+
+public interface IActionInterfaceProvider : BaseIActionInterfaceProvider
+{
+
+
+} // end main interface 
+
+} // end namespace 
---
+++ br.ufc.mdcc.hpc.storm.binding.context.EnvironmentInterface/src/1.0.0.0/IEnvironmentInterfaceProvider.cs
@@ -0,0 +1,12 @@
+using br.ufc.pargo.hpe.kinds;
+
+namespace br.ufc.mdcc.hpc.storm.binding.context.EnvironmentInterface { 
+
+public interface IEnvironmentInterface : BaseIEnvironmentInterface
+{
+	
+
+} // end main interface 
+
+
+} // end namespace 
